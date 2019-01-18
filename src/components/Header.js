5ec1ce48f146e4132014368cde1d70103aa16b74
sergiorock <lopezer.nic@gmail.com>
2019-01-18 06:39:19 -0300

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Header = props => {
+    return (
+        <header className="top">
+            <h1>Cotizador de seguros de auto</h1>
+        </header>
+    )
+}
+export default Header;
